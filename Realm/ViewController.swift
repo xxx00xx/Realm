146566a375129d00fcd2f752db09bace3990b72f
Shotaro Furuta <zoxx31syg1y999@gmail.com>
2020-10-01 23:53:53 +0900

    Initial Commit

---
+++ Realm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Realm
+//
+//  Created by 古田翔太郎 on 2020/10/01.
+//  Copyright © 2020 古田翔太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
